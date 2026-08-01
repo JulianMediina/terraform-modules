a381259c7be4c187ff2668aa3d3be07d8372dca3
JulianMediina <julian.mediina@gmail.com>
2026-08-01 17:16:02 -0500

    Habilita Container Insights en el cluster ECS
    checkov (CKV_AWS_65) lo exige; ademas da metricas de tareas activas
    que el dashboard de observability puede usar mas adelante.

--- modules/ecs-express/main.tf
+++ modules/ecs-express/main.tf
@@ -46,6 +46,12 @@ resource "aws_iam_role_policy_attachment" "infrastructure" {
 
 resource "aws_ecs_cluster" "site" {
   name = "daviplata-${var.environment}"
+
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+
   tags = var.tags
 }
 
